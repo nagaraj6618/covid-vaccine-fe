9b8f92f2a81c1ddd20b493656af9cc236aeaec71
NAGARAJ S <nagaraj516700@gmail.com>
2024-05-19 00:28:50 +0530

    Feat : Added Filter option

--- src/Components/Centers/CenterComponent.jsx
+++ src/Components/Centers/CenterComponent.jsx
@@ -4,57 +4,62 @@ import { BE_URL } from '../../info';
 import { Link } from 'react-router-dom';
 const CenterComponent = () => {
   const [isAdmin, setIsAdmin] = useState(false);
-  const [centerData, setCenterData] = useState('');
+  const [centerData, setCenterData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [filter, setFilter] = useState('');
+
   async function fetchAllCenter() {
     try {
       const response = await axios.get(`${BE_URL}/center`);
       console.log(response.data);
-      setCenterData(response.data);
-    }
-    catch (error) {
+      setCenterData(response.data.data);
+      setFilteredData(response.data.data); // Initialize filteredData with the full data set
+    } catch (error) {
       console.log(error.response.data);
       if (error.response) {
-        setCenterData(error.response.data);
+        setCenterData([]);
+        setFilteredData([]);
       }
     }
-
   }
 
   const centerDeleteHandler = async (id) => {
-    console.log(id)
+    console.log(id);
     try {
       const token = sessionStorage.getItem('token');
       const response = await axios.delete(`${BE_URL}/center/${id}`, {
         headers: {
           token: token,
-          Authorization:`Bearer ${token}`
+          Authorization: `Bearer ${token}`
         }
-      })
+      });
       console.log(response.data);
       fetchAllCenter();
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
+  };
 
-  }
   useEffect(() => {
     fetchAllCenter();
     const userRole = localStorage.getItem('role');
-    if (userRole === 'admin') {
-      setIsAdmin(true);
-    }
-    else {
-      setIsAdmin(false);
-    }
-  }, [])
+    setIsAdmin(userRole === 'admin');
+  }, []);
+
+  useEffect(() => {
+    
+    const filtered = centerData.filter(center =>
+      center.name.toLowerCase().includes(filter.toLowerCase()) ||
+      center.email.toLowerCase().includes(filter.toLowerCase()) ||
+      center.phone.toLowerCase().includes(filter.toLowerCase())
+    );
+    setFilteredData(filtered);
+  }, [filter, centerData]);
+
   return (
     <div className='center-main-container'>
-
-
-
       <div className='flex align-middle justify-center'>
-        {!centerData && (
+        {!centerData.length && (
           <div className="flex items-center justify-center w-full h-40">
             <svg className="animate-spin h-8 w-8 text-slate-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
@@ -64,65 +69,72 @@ const CenterComponent = () => {
           </div>
         )}
       </div>
-      <div className="container mx-auto p-4">
-        {
-          centerData && centerData.data.map((data, index) => (
-            <div className="center-container bg-white shadow-lg rounded-lg p-6 mb-6" key={index}>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div>
-                  <p className="font-semibold text-violet-700">Center Name: <span className="text-gray-900 font-bold ">{data.name}</span></p>
-                </div>
-                <div>
-                  <p className="font-semibold text-violet-700">Contact Mail: <span className="text-gray-900">{data.email}</span></p>
-                </div>
-                <div>
-                  <p className="font-semibold text-violet-700">Contact Ph: <span className="text-gray-900">{data.phone}</span></p>
-                </div>
-                <div>
-                  <p className="font-semibold text-violet-700">Address: <span className="text-gray-900">{data.address.place}</span></p>
-                </div>
-                <div>
-                  <p className="font-semibold text-violet-700">Pincode & City: <span className="text-gray-900">{`${data.address.pincode},${data.address.city}`}</span></p>
-                </div>
-                <div>
-                  <p className="font-semibold text-violet-700">State: <span className="text-gray-900">{data.address.state}</span></p>
-                </div>
-                <div className="md:col-span-2">
-                  <p className="font-semibold text-violet-700">Opening Hours: <span className="text-gray-900">{data.openingHours}</span></p>
-                </div>
-                <div>
-                  <p className="font-semibold text-violet-700">Active Status: <span className="text-gray-900 font-bold ">{data.status?'Active':'Inactive'}</span></p>
-                </div>
-                <div className="md:col-span-2">
-                  <p className="font-semibold text-white "> <Link to={`/center/${data._id}`} className='bg-violet-700 p-3 rounded'>Veiw Details </Link></p>
-                </div>
-                {isAdmin &&
-                  <div className="md:col-span-2 m-3">
-                    <p className="font-semibold text-white ">
-                      <Link to={`/center/update/${data._id}`} className='text-violet-700 p-3 rounded'>Update </Link>
-                      <button className='btn' onClick={() => centerDeleteHandler(data._id)}>Delete</button>
-                    </p>
 
-                  </div>
-                }
+      <div className="container mx-auto p-4">
+        <div className="flex justify-end mb-4">
+          <input
+            type="text"
+            placeholder="Filter by name, email, or phone"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            className="border p-2 rounded"
+          />
+        </div>
+
+        {filteredData.map((data, index) => (
+          <div className="center-container bg-white shadow-lg rounded-lg p-6 mb-6" key={index}>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div>
+                <p className="font-semibold text-violet-700">Center Name: <span className="text-gray-900 font-bold ">{data.name}</span></p>
               </div>
+              <div>
+                <p className="font-semibold text-violet-700">Contact Mail: <span className="text-gray-900">{data.email}</span></p>
+              </div>
+              <div>
+                <p className="font-semibold text-violet-700">Contact Ph: <span className="text-gray-900">{data.phone}</span></p>
+              </div>
+              <div>
+                <p className="font-semibold text-violet-700">Address: <span className="text-gray-900">{data.address.place}</span></p>
+              </div>
+              <div>
+                <p className="font-semibold text-violet-700">Pincode & City: <span className="text-gray-900">{`${data.address.pincode},${data.address.city}`}</span></p>
+              </div>
+              <div>
+                <p className="font-semibold text-violet-700">State: <span className="text-gray-900">{data.address.state}</span></p>
+              </div>
+              <div className="md:col-span-2">
+                <p className="font-semibold text-violet-700">Opening Hours: <span className="text-gray-900">{data.openingHours}</span></p>
+              </div>
+              <div>
+                <p className="font-semibold text-violet-700">Active Status: <span className="text-gray-900 font-bold ">{data.status ? 'Active' : 'Inactive'}</span></p>
+              </div>
+              <div className="md:col-span-2">
+                <p className="font-semibold text-white "><Link to={`/center/${data._id}`} className='bg-violet-700 p-3 rounded'>View Details</Link></p>
+              </div>
+              {isAdmin &&
+                <div className="md:col-span-2 m-3">
+                  <p className="font-semibold text-white ">
+                    <Link to={`/center/update/${data._id}`} className='text-violet-700 p-3 rounded'>Update</Link>
+                    <button className='btn' onClick={() => centerDeleteHandler(data._id)}>Delete</button>
+                  </p>
+                </div>
+              }
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
+
       {isAdmin &&
         <div className="flex justify-center items-center m-3">
           <div className="md:col-span-2 m-3">
             <p className="font-semibold text-white">
               <Link to={`/center/new`} className="bg-violet-700 p-3 rounded">Add new</Link>
             </p>
-
           </div>
         </div>
       }
-
     </div>
-  )
+  );
 }
 
 export default CenterComponent
