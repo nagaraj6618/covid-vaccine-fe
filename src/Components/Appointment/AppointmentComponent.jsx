41891be79e1f6ba968663d0240cc979f1037318c
NAGARAJ S <nagaraj516700@gmail.com>
2024-05-21 22:13:47 +0530

    feat : Added filter option in Appointment history

--- src/Components/Appointment/AppointmentComponent.jsx
+++ src/Components/Appointment/AppointmentComponent.jsx
@@ -7,22 +7,24 @@ import { Link } from 'react-router-dom';
 const AppointmentComponent = () => {
   const [responseData, setResponseData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [filterOption, setFilterOption] = useState({
+    date: '',
+    status: '',
+  });
 
   const fetchAllAppointmentHistory = async () => {
     setLoading(true);
     try {
       const token = sessionStorage.getItem('token');
-      const response = await axios.get(`${BE_URL}/book/user`, {
+      const response = await axios.get(`${BE_URL}/book/user?status=${filterOption.status}&date=${filterOption.date}`, {
         headers: {
           Authorization: `Bearer ${token}`,
-          token: token
-        }
+        },
       });
       console.log(response.data);
       setResponseData(response.data);
       setLoading(false);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error.response);
       setLoading(false);
       if (error.response) {
@@ -33,10 +35,44 @@ const AppointmentComponent = () => {
 
   useEffect(() => {
     fetchAllAppointmentHistory();
-  }, []);
+  }, [filterOption]);
+
+  const inputFilterHandler = (e) => {
+    setFilterOption((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 py-8">
+      <div className="max-w-3xl mx-auto mb-6">
+        <div className="flex justify-between items-center mb-4">
+          <div>
+            <label htmlFor="status" className="block text-sm font-medium text-gray-700">
+              Filter by Status:
+            </label>
+            <select
+              onChange={inputFilterHandler}
+              id="status"
+              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            >
+              <option value="">All</option>
+              <option value="Scheduled">Scheduled</option>
+              <option value="Pending">Pending</option>
+              <option value="Completed">Completed</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="date" className="block text-sm font-medium text-gray-700">
+              Filter by Date:
+            </label>
+            <input
+              type="date"
+              onChange={inputFilterHandler}
+              id="date"
+              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            />
+          </div>
+        </div>
+      </div>
       <div className='flex justify-center'>
         {loading && (
           <div className="flex items-center justify-center w-full h-40">
@@ -50,23 +86,39 @@ const AppointmentComponent = () => {
       </div>
       {responseData && responseData.success === false && <ErrorMessage error={responseData.message} />}
 
+      <div className="max-w-3xl mx-auto mb-6">
+        {responseData && responseData.success === true && responseData.data.length === 0 && (
+          <div className="text-center text-gray-500">
+            No Record Found
+          </div>
+        )}
+        {responseData && responseData.success === true && (
+          <div className="text-black text-center mb-6">
+            No of records: {responseData.data.length}
+          </div>
+        )}
+      </div>
+
       <div className="max-w-3xl mx-auto">
         {responseData && responseData.success === true && responseData.data.map((data, index) => (
           <div key={index} className="bg-white shadow-lg rounded-lg p-6 mb-4">
             <div className="mb-2">
-              <p className="text-lg font-semibold">Appointment Date: <span className="font-normal">{new Date(data.appointmentDate).toDateString()}</span></p>
+              <p className="text-lg font-semibold">
+                Appointment Date: <span className="font-normal">{new Date(data.appointmentDate).toDateString()}</span>
+              </p>
             </div>
             <div className="mb-2">
-              <p className="text-lg font-semibold">Status: <span className="font-normal">{data.status}</span></p>
+              <p className="text-lg font-semibold">
+                Status: <span className="font-normal">{data.status}</span>
+              </p>
             </div>
             <div>
-              <Link to={`/patient/${data.patientId}`} className='inline-block bg-blue-500 text-white px-4 py-2 rounded m-2 hover:bg-blue-600 transition duration-300'>
+              <Link to={`/patient/${data.patientId}`} className="inline-block bg-blue-500 text-white px-4 py-2 rounded m-2 hover:bg-blue-600 transition duration-300">
                 View Patient Details
               </Link>
             </div>
-
             <div>
-              <Link to={`/center/${data.centerId}`} className='inline-block btn bg-blue-500 text-white px-4 py-2 rounded m-2 hover:bg-blue-600 transition duration-300'>
+              <Link to={`/center/${data.centerId}`} className="inline-block  bg-violet-700 text-white px-4 py-2 rounded m-2 hover:bg-blue-600 transition duration-300">
                 View Center Details
               </Link>
             </div>
