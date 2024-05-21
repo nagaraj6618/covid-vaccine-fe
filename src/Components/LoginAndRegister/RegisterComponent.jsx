d516cb59045f835d14e98df5bd3d9fcb8918c549
NAGARAJ S <nagaraj516700@gmail.com>
2024-05-21 23:40:39 +0530

    Fix : Login loading animation fixed

--- src/Components/LoginAndRegister/RegisterComponent.jsx
+++ src/Components/LoginAndRegister/RegisterComponent.jsx
@@ -6,7 +6,7 @@ import { BE_URL } from '../../info';
 import axios from 'axios'
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import SuccessMessageComponent from '../SuccessMessage/SuccessMessageComponent';
-import LoadingComponent from '../LoadingComponent/LoadingComponent';
+// import LoadingComponent from '../LoadingComponent/LoadingComponent';
 
 const RegisterComponent = () => {
   const navigate = useNavigate();
@@ -76,18 +76,18 @@ const RegisterComponent = () => {
       }
 
       {responseData && responseData.success === false && <ErrorMessage error={responseData.message} />}
-
+      {/* {!loading && <LoadingComponent/>} */}
       <div className='flex '>
-        {/* {!loading && (
+        {!loading && (
           <div className="flex  h-40 absolute top-20 left-10 z-10">
-            <svg className="animate-spin h-8 w-8 text-slate-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <svg className="animate-spin h-8 w-8 text-violet-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20 12a8 8 0 01-8 8v4c6.627 0 12-5.373 12-12h-4zm-2-5.291c1.865 2.114 3 4.896 3 7.938h4c0-6.627-5.373-12-12-12v4z"></path>
             </svg>
             <span className="ml-2">Loading...</span>
           </div>
-        )} */}
-        {!loading && <LoadingComponent/>}
+        )}
+        
       </div>
       <form onSubmit={formSubmitHandler} className='loginregisterform-container'>
 
